bee16c62124b9b916ad0061a86e5539fa8e69ae3
Early-Birdd <goqhwk1100@nate.com>
2022-11-13 00:14:23 +0900

    Initial commit

---
+++ src/main/java/com/example/cardbatch/CardBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cardbatch/CardBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
